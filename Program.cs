dcf2114b33a39bc03b8451dff048b355248b724b
zpsle <zpsle@DESKTOP-ELOIINN>
2023-01-17 20:06:50 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Solution (1): Hello, World!");
+
+string stg = "Hello, World!";
+Console.WriteLine("solution (2): " + stg);
+Console.WriteLine(" ");
+
+Console.WriteLine("Enter a user's name: ");
+string userName = Console.ReadLine();
+Console.WriteLine("The user's name is: " + userName);
+Console.WriteLine(" ");
+
+Console.WriteLine("Enter the user's age: ")
+//int age = Convert.ToInt32(Console.Readline());
+int age = int.Parse(Console.ReadLine());
+Console.WriteLine("The user's age is: " + age)
